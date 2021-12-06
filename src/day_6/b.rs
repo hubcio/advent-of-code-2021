02d8b8a8f4da802fc6461713fed5488dbbea7c1d
Hubert Gruszecki <hubert.gruszecki@nokia.com>
2021-12-06 15:27:06 +0100

    Improve day 6

--- src/day_6/b.rs
+++ src/day_6/b.rs
@@ -17,15 +17,8 @@ pub fn find_fish_amount(file_path: PathBuf, days: usize) -> usize {
 
     for _ in 0..days {
         let zero_day_fish = f[0];
-        f[0] = f[1];
-        f[1] = f[2];
-        f[2] = f[3];
-        f[3] = f[4];
-        f[4] = f[5];
-        f[5] = f[6];
-        f[6] = f[7] + zero_day_fish;
-        f[7] = f[8];
-        f[8] = zero_day_fish;
+        f.rotate_left(1);
+        f[6] += zero_day_fish;
     }
     f.iter().sum()
 }
